fd8e7a7be79460f0723f00ecde3e9c3d5eb26307
Jakob Selberg <jakobselberg@gmail.com>
2026-05-20 23:21:24 +0200

    Rename term file to terms and split off header file

--- terms.hpp
+++ terms.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <string>
 #include <variant>
 #include <vector>
@@ -13,7 +14,6 @@ struct Variable
 };
 
 struct FunctionApplication;
-
 using Term = std::variant<Variable, std::shared_ptr<const FunctionApplication>>;
 
 struct FunctionSymbol
@@ -21,29 +21,13 @@ struct FunctionSymbol
     int arity;
     std::string name;
 };
-
 struct FunctionApplication
 {
     FunctionSymbol symbol;
     std::vector<Term> arguments;
 };
 
-std::set<Variable> FreeVariables(const Term &t)
-{
-    if (auto v = std::get_if<Variable>(&t))
-        return {*v};
-    const auto &f = *std::get<std::shared_ptr<const FunctionApplication>>(t);
-    std::set<Variable> result;
-    for (const auto &arg : f.arguments)
-    {
-        auto sub = FreeVariables(arg);
-        result.merge(sub);
-    }
-    return result;
-};
-
 using Substitution = std::map<Variable, Term>;
 
-Term substitute(const Substitution &substitution, const Term &t)
-{
-}
+std::set<Variable> FreeVariables(const Term &t);
+Term makeApp(FunctionSymbol symbol, std::vector<Term> arguments);
