7fc13bc48a9781500e779f8460ca7edc7bb46441
Jakob Selberg <jakobselberg@gmail.com>
2026-06-29 15:09:32 +0200

    Add saturation utility function headers and saturation procedure signature

--- include/saturation.hpp
+++ include/saturation.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "cnf.hpp"
 #include "inference.hpp"
+#include <cstddef> // for std::size_t
 
 enum class SaturationResult
 {
@@ -15,3 +16,12 @@ struct ProofState
     std::vector<Clause> passive;
     int next_id;
 };
+
+// models a contradiction
+bool isEmptyClause(const Clause &c);
+
+int clauseWeight(const Clause &c);
+
+std::size_t selectGivenIndex(const std::vector<Clause> &passive);
+
+SaturationResult saturate(ProofState &state, int max_iteration = 10000);
