43b1fb076e76b47a25bd85c9d4522d3f7d812512
Jakob Selberg <jakobselberg@gmail.com>
2026-07-06 17:21:50 +0200

    Add domination check for kbo

--- src/lib/ordering.cpp
+++ src/lib/ordering.cpp
@@ -28,12 +28,27 @@ void collectVariableCounts(const Term &t, std::map<Variable, int> &counts)
         collectVariableCounts(arg, counts);
 };
 
+// does 'super' contains at least as many occurences of every variable as 'sub'?
+bool variableDominates(const std::map<Variable, int> &super, const std::map<Variable, int> &sub)
+{
+    for (const auto &[v, n] : sub)
+    {
+        auto it = super.find(v);
+        const int have = (it == super.end()) ? 0 : it->second;
+        if (have < n)
+            return false;
+    }
+    return true;
+}
+
 Comparison kboCompare(const Term &s, const Term &t)
 {
     std::map<Variable, int> sCounts;
     std::map<Variable, int> tCounts;
     collectVariableCounts(s, sCounts);
     collectVariableCounts(t, tCounts);
+
+    const bool sDominates = variablesDominate(sCounts, tCounts);
 
     const int ws = termWeight(s);
     const int wt = termWeight(t);
